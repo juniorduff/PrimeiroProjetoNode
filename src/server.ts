739ac131d5b3bf4137436b0fc762df94f6d710bc
Valmir junior <junior_duff_sm@hotmail.com>
2020-06-11 20:20:29 -0300

    frist comit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express, { json } from 'express';
+// @ts-ignore
+import routes from './routes';
+
+const app = express();
+app.use(json());
+app.use(routes);
+
+app.listen(3000, () => {
+  console.log('listening on http://localhost:3000');
+});
